49071b12268a8698c098d73adb495b85924feefa
AI-skimos <send@hailang.email>
2023-05-12 22:36:12 +0800

    dev

--- all.sh
+++ all.sh
@@ -2,7 +2,8 @@
 
 env PYTHONDONTWRITEBYTECODE=1 &>/dev/null
 env TF_CPP_MIN_LOG_LEVEL=1 &>/dev/null
-BASEPATH=/content/drive/MyDrive/SD
+#BASEPATH=/content/drive/MyDrive/SD
+BASEPATH=/tmp/SD
 
 function safe_git {
   if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
@@ -77,7 +78,6 @@ function reset_repos {
         return 1
     fi
     local base_path="$1" all=${2:-""}
-    #cd $base_path && pwd && git reset --hard && git pull
     git -C $base_path reset --hard
     git -C $base_path/repositories/stable-diffusion-stability-ai reset --hard
     if [ "$all" = "all" ]; then
@@ -108,7 +108,8 @@ function sed_for {
     fi
     sed -i -e 's/checkout {commithash}/checkout --force {commithash}/g' $base_dir/launch.py
 }
-function install_perf_tools {
+
+function prepare_perf_tools {
     local packages=("http://launchpadlibrarian.net/367274644/libgoogle-perftools-dev_2.5-2.2ubuntu3_amd64.deb" "https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/google-perftools_2.5-2.2ubuntu3_all.deb" "https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/libtcmalloc-minimal4_2.5-2.2ubuntu3_amd64.deb" "https://launchpad.net/ubuntu/+source/google-perftools/2.5-2.2ubuntu3/+build/14795286/+files/libgoogle-perftools4_2.5-2.2ubuntu3_amd64.deb")
     regex="/([^/]+)\.deb$"
     install=false
@@ -134,44 +135,216 @@ function install_perf_tools {
     $install && dpkg -i /tmp/*.deb
 }
 
-function install {
-    #Prepare runtime
-    safe_git https://github.com/camenduru/stable-diffusion-webui $BASEPATH v2.1
-    safe_git https://huggingface.co/embed/negative $BASEPATH/embeddings/negative
-    safe_git https://huggingface.co/embed/lora $BASEPATH/models/Lora/positive
-    safe_fetch https://huggingface.co/embed/upscale/resolve/main/4x-UltraSharp.pth $BASEPATH/models/ESRGAN 4x-UltraSharp.pth
-    safe_fetch https://raw.githubusercontent.com/camenduru/stable-diffusion-webui-scripts/main/run_n_times.py $BASEPATH/scripts run_n_times.py
-    safe_git https://github.com/deforum-art/deforum-for-automatic1111-webui $BASEPATH/extensions/deforum-for-automatic1111-webui
-    safe_git https://github.com/camenduru/stable-diffusion-webui-images-browser $BASEPATH/extensions/stable-diffusion-webui-images-browser
-    safe_git https://github.com/camenduru/stable-diffusion-webui-huggingface $BASEPATH/extensions/stable-diffusion-webui-huggingface
-    safe_git https://github.com/camenduru/sd-civitai-browser $BASEPATH/extensions/sd-civitai-browser
-    safe_git https://github.com/kohya-ss/sd-webui-additional-networks $BASEPATH/extensions/sd-webui-additional-networks
-    safe_git https://github.com/Mikubill/sd-webui-controlnet $BASEPATH/extensions/sd-webui-controlnet
-    safe_git https://github.com/fkunn1326/openpose-editor $BASEPATH/extensions/openpose-editor
-    safe_git https://github.com/jexom/sd-webui-depth-lib $BASEPATH/extensions/sd-webui-depth-lib
-    safe_git https://github.com/hnmr293/posex $BASEPATH/extensions/posex
-    safe_git https://github.com/nonnonstop/sd-webui-3d-open-pose-editor $BASEPATH/extensions/sd-webui-3d-open-pose-editor
-    safe_git https://github.com/camenduru/sd-webui-tunnels $BASEPATH/extensions/sd-webui-tunnels
-    safe_git https://github.com/etherealxx/batchlinks-webui $BASEPATH/extensions/batchlinks-webui
-    safe_git https://github.com/camenduru/stable-diffusion-webui-catppuccin $BASEPATH/extensions/stable-diffusion-webui-catppuccin
-    safe_git https://github.com/KohakuBlueleaf/a1111-sd-webui-locon $BASEPATH/extensions/a1111-sd-webui-locon
-    safe_git https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg $BASEPATH/extensions/stable-diffusion-webui-rembg
-    safe_git https://github.com/ashen-sensored/stable-diffusion-webui-two-shot $BASEPATH/extensions/stable-diffusion-webui-two-shot
-    safe_git https://github.com/camenduru/sd_webui_stealth_pnginfo $BASEPATH/extensions/sd_webui_stealth_pnginfo
-    safe_git https://github.com/thomasasfk/sd-webui-aspect-ratio-helper  $BASEPATH/extensions/sd-webui-aspect-ratio-helper
-    safe_git https://github.com/dtlnor/stable-diffusion-webui-localization-zh_CN $BASEPATH/extensions/stable-diffusion-webui-localization-zh_CN
-    safe_git https://github.com/AI-skimos/sd-webui-prompt-sr-range $BASEPATH/extensions/sd-webui-prompt-sr-range
-    reset_repos $BASEPATH all
+function assemble_path {
+    if [[ "$#" -ne 1 ]]; then
+      echo "Usage: assemble_path <type>"
+      return 1
+    fi
+    local type=$1
+    case $type in
+      webui)
+        echo $BASEPATH
+        ;;
+      extension)
+        echo $BASEPATH/extensions
+        ;;
+      extra_script)
+        echo $BASEPATH/scripts
+        ;;
+      embedding)
+        echo $BASEPATH/embeddings
+        ;;
+      ESRGAN_model)
+        echo $BASEPATH/models/ESRGAN
+        ;;
+      checkpoint)
+        echo $BASEPATH/models/Stable-Diffusion
+        ;;
+      hypernetwork)
+        echo $BASEPATH/models/hypernetworks
+        ;;
+      lora)
+        echo $BASEPATH/models/Lora
+        ;;
+      lycoris)
+        echo $BASEPATH/models/LyCORIS
+        ;;
+      vae)
+        echo $BASEPATH/models/VAE
+        ;;
+      clip)
+        echo $BASEPATH/models/CLIP
+        ;;
+      cn_model)
+        echo $BASEPATH/extensions/sd-webui-controlnet/models
+        ;;
+      *)
+        echo "Error: Unrecognized template config type $type."
+        exit 1
+        ;;
+    esac
+}
+assemble_path webui
+assemble_path hypernetwork
+assemble_path ESRGAN_model
+assemble_path extra_script
+assemble_path embedding
+assemble_path extension
+assemble_path model
+assemble_path lora
+assemble_path lycoris
+assemble_path vae
+function install_from_template {
+    if [ "$#" -lt 2 ]; then
+      echo "Usage: install_from_template <template_path> <type>"
+      return 1
+    fi
+    local template_path=$1
+    local type=$2
 
-    #Download Controlnet Models
+    config_path=$template_path/$type.txt
+    if [[ -f $config_path ]]; then
+      mapfile -t lines < $config_path
+      for line in "${lines[@]}"
+      do
+          trimmed_url=$(echo "$trimmed" | tr -d ' ' | cut -d "#" -f 1)
+          base_name=$(basename $trimmed_url)
+          if [[ -z $trimmed_url ]]; then
+            git ls-remote $trimmed_url &> /dev/null
+            if [[ $? -eq 0 ]]; then
+              #this is a git repo
+              branch=$(grep -q "#" && echo "${line#*#}")
+              safe_git "$trimmed_url" $(assemble_path $type)/$base_name ${branch:+$branch}
+            else
+              safe_fetch $trimmed_url $(assemble_path $type) $base_name
+            fi
+          fi
+      done
+    fi
+}
+
+config_path=templates/camenduru
+function install_webui {
+    path=$config_path/webui.txt
+    if [[ -f $path ]]; then
+      mapfile -t lines < $path
+      if [[ ${#lines[@]} -ge 1 ]]; then
+        repo=$(echo "${lines[0]}" | cut -d "#" -f 1)
+        branch=$(echo "${lines[0]}" | grep -q "#" && echo "${lines[0]}" | cut -d "#" -f 2)
+        safe_git "$repo" "$BASEPATH" ${branch:+$branch}
+      else
+        echo "Error: wrong format for template config file: $path"
+      fi
+    else
+      safe_git https://github.com/AUTOMATIC1111/stable-diffusion-webui $BASEPATH
+    fi
+}
+#install_webui
+
+function install_embeddings {
+    path=$config_path/embeddings.txt
+    if [[ -f $path ]]; then
+      mapfile -t lines < $path
+      for line in "${lines[@]}"
+      do
+          repo=$(echo "${lines[0]}" | cut -d "#" -f 1)
+          branch=$(echo "${lines[0]}" | grep -q "#" && echo "${lines[0]}" | cut -d "#" -f 2)
+          safe_git "$repo" "$BASEPATH/embeddings" ${branch:+$branch}
+      done
+    fi
+}
+#install_embeddings
+
+function install_ESRGAN_models {
+    path=$config_path/ESRGAN_models.txt
+    if [[ -f $path ]]; then
+      mapfile -t lines < $path
+      for line in "${lines[@]}"
+      do
+          safe_fetch $line $BASEPATH/models/ESRGAN $(echo "$line" | awk -F/ '{print $NF}')
+      done
+    fi
+}
+
+function install_extra_scripts {
+    path=$config_path/extra_scripts.txt
+    if [[ -f $path ]]; then
+      mapfile -t lines < $path
+      for line in "${lines[@]}"
+      do
+          safe_fetch $line $BASEPATH/scripts $(echo "$line" | awk -F/ '{print $NF}')
+      done
+    fi
+}
+
+function install_extensions {
+    safe_git https://github.com/deforum-art/deforum-for-automatic1111-webui $BASEPATH/extensions/deforum-for-automatic1111-webui
+    path=$config_path/extensions.txt
+    if [[ -f $path ]]; then
+      mapfile -t lines < $path
+      for line in "${lines[@]}"
+      do
+          repo=$(echo "${lines[0]}" | cut -d "#" -f 1)
+          branch=$(echo "${lines[0]}" | grep -q "#" && echo "${lines[0]}" | cut -d "#" -f 2)
+          safe_git "$repo" "$BASEPATH/extensions" ${branch:+$branch}
+      done
+    fi
+}
+
+function install_cn_models {
     safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11f1e_sd15_tile_fp16.safetensors
     #safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11p_sd15_canny_fp16.safetensors
     #safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11p_sd15_openpose_fp16.safetensors
     #safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_seg_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11p_sd15_seg_fp16.safetensors
+}
 
+function install_checkpoints {
     #Download Model & VAE
     safe_fetch https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors $BASEPATH/models/Stable-diffusion Deliberate_v2.safetensors
+
+}
+
+function install_vae {
     safe_fetch https://huggingface.co/Yukihime256/840000/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt $BASEPATH/models/VAE vae-ft-mse-840000-ema-pruned.ckpt
+}
+
+function prepare_pip_deps {
+    pip install -q torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U
+    pip install -q xformers==0.0.18 triton==2.0.0 -U
+}
+
+function prepare_fuse_dir {
+    #Prepare drive fusion MOVE THIS TOLATER
+    mkdir /content/fused-models
+    mkdir /content/models
+    mkdir /content/fused-lora
+    mkdir /content/lora
+    unionfs-fuse $BASEPATH/models/Stable-diffusion=RW:/content/models=RW /content/fused-models
+    unionfs-fuse $BASEPATH/extensions/sd-webui-additional-networks/models/lora=RW:$BASEPATH/models/Lora=RW:/content/lora=RW /content/fused-lora
+}
+
+function prepare_clip {
+    mkdir -p $BASEPATH/models/CLIP
+    safe_fetch https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt $BASEPATH/models/CLIP ViT-L-14.pt
+}
+
+function install {
+    #Prepare runtime
+    install_webui
+    install_embeddings
+    install_ESRGAN_models
+    install_extra_scripts
+    install_cn_models
+    install_extensions
+
+    install_from_template camenduru webui
+    install_from_template camenduru embeddings
+
+
+    reset_repos $BASEPATH all
+
+    install_checkpoints
+    install_vae
 
     #Install Dependencies
     sed_for installation $BASEPATH
@@ -180,19 +353,10 @@ function install {
 
 function run {
     #Install google perf tools
-    install_perf_tools
+    prepare_perf_tools
 
-    #Install pip dependencies
-    pip install -q torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U
-    pip install -q xformers==0.0.18 triton==2.0.0 -U
-
-    #Prepare drive fusion MOVE THIS TOLATER
-    mkdir /content/fused-models
-    mkdir /content/models
-    mkdir /content/fused-lora
-    mkdir /content/lora
-    unionfs-fuse $BASEPATH/models/Stable-diffusion=RW:/content/models=RW /content/fused-models
-    unionfs-fuse $BASEPATH/extensions/sd-webui-additional-networks/models/lora=RW:$BASEPATH/models/Lora=RW:/content/lora=RW /content/fused-lora
+    prepare_pip_deps
+    prepare_fuse_dir
 
     #Healthcheck and update all repos
     reset_repos $BASEPATH
@@ -200,12 +364,9 @@ function run {
     #Prepare for running
     sed_for run $BASEPATH
 
-    #Make sure CLIP folder exists and downloads the model if not present
-    mkdir -p $BASEPATH/models/CLIP
-    safe_fetch https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt $BASEPATH/models/CLIP ViT-L-14.pt
- 
-    cd $BASEPATH
-    python launch.py --listen --opt-sdp-attention --enable-insecure-extension-access --theme dark --gradio-queue --clip-models-path $BASEPATH/models/CLIP --multiple
+    prepare_clip
+
+    cd $BASEPATH && python launch.py --listen --opt-sdp-attention --enable-insecure-extension-access --theme dark --gradio-queue --clip-models-path $BASEPATH/models/CLIP --multiple
 }
 
 force=false
@@ -222,10 +383,10 @@ while getopts "f" opt; do
 done
 
 #Update packages
-apt -y update -qq && apt -y install -qq unionfs-fuse libcairo2-dev pkg-config python3-dev
+#apt -y update -qq && apt -y install -qq unionfs-fuse libcairo2-dev pkg-config python3-dev
 
-if [ "$force" = true ] || [ ! -e "$BASEPATH/.install_status" ] || ! grep -qs "Installation Completed" "$BASEPATH/.install_status"; then
-    install
-fi
+#if [ "$force" = true ] || [ ! -e "$BASEPATH/.install_status" ] || ! grep -qs "Installation Completed" "$BASEPATH/.install_status"; then
+#    install
+#fi
 
-run
+#run
