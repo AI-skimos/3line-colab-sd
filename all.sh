bdeb070e93f04634c3a9dfcd3118b3d56713b969
AI-skimos <send@hailang.email>
2023-05-13 00:30:02 +0800

    testing default template

--- all.sh
+++ all.sh
@@ -135,9 +135,9 @@ function prepare_perf_tools {
     $install && dpkg -i /tmp/*.deb
 }
 
-function assemble_path {
+function assemble_target_path {
     if [[ "$#" -ne 1 ]]; then
-      echo "Usage: assemble_path <type>"
+      echo "Usage: assemble_target_path <type>"
       return 1
     fi
     local type=$1
@@ -145,22 +145,22 @@ function assemble_path {
       webui)
         echo $BASEPATH
         ;;
-      extension)
+      extensions)
         echo $BASEPATH/extensions
         ;;
-      extra_script)
+      scripts)
         echo $BASEPATH/scripts
         ;;
-      embedding)
+      embeddings)
         echo $BASEPATH/embeddings
         ;;
-      ESRGAN_model)
+      ESRGAN_models)
         echo $BASEPATH/models/ESRGAN
         ;;
-      checkpoint)
-        echo $BASEPATH/models/Stable-Diffusion
+      checkpoints)
+        echo $BASEPATH/models/Stable-diffusion
         ;;
-      hypernetwork)
+      hypernetworks)
         echo $BASEPATH/models/hypernetworks
         ;;
       lora)
@@ -175,7 +175,7 @@ function assemble_path {
       clip)
         echo $BASEPATH/models/CLIP
         ;;
-      cn_model)
+      cn_models)
         echo $BASEPATH/extensions/sd-webui-controlnet/models
         ;;
       *)
@@ -184,128 +184,40 @@ function assemble_path {
         ;;
     esac
 }
-assemble_path webui
-assemble_path hypernetwork
-assemble_path ESRGAN_model
-assemble_path extra_script
-assemble_path embedding
-assemble_path extension
-assemble_path model
-assemble_path lora
-assemble_path lycoris
-assemble_path vae
+
 function install_from_template {
-    if [ "$#" -lt 2 ]; then
-      echo "Usage: install_from_template <template_path> <type>"
+    if [ "$#" -ne 1 ]; then
+      echo "Usage: install_from_template <config_path>"
       return 1
     fi
-    local template_path=$1
-    local type=$2
-
-    config_path=$template_path/$type.txt
+    local config_path=$1
     if [[ -f $config_path ]]; then
       mapfile -t lines < $config_path
       for line in "${lines[@]}"
       do
-          trimmed_url=$(echo "$trimmed" | tr -d ' ' | cut -d "#" -f 1)
+          echo "->Processing template config line: $line"
+          trimmed_url=$(echo "$line" | tr -d ' ' | cut -d "#" -f 1)
           base_name=$(basename $trimmed_url)
-          if [[ -z $trimmed_url ]]; then
+          if [[ ! -z $trimmed_url ]]; then
+            type=$(basename $config_path | cut -d "." -f 1)
             git ls-remote $trimmed_url &> /dev/null
             if [[ $? -eq 0 ]]; then
               #this is a git repo
-              branch=$(grep -q "#" && echo "${line#*#}")
-              safe_git "$trimmed_url" $(assemble_path $type)/$base_name ${branch:+$branch}
+              branch=$(echo "$line" | grep -q "#" && echo "$line" | cut -d "#" -f 2)
+              echo "->This is a $type component Git repo with branch $branch, will be saved to $(assemble_target_path $type)"
+              if [[ $type == "webui" ]]; then
+                safe_git "$trimmed_url" $(assemble_target_path $type) ${branch:+$branch}
+                break
+              else
+                safe_git "$trimmed_url" $(assemble_target_path $type)/$base_name ${branch:+$branch}
+              fi
             else
-              safe_fetch $trimmed_url $(assemble_path $type) $base_name
+              echo "->This is a $type component file, will be saved to $(assemble_target_path $type)"
+              safe_fetch $trimmed_url $(assemble_target_path $type) $base_name
             fi
           fi
       done
     fi
-}
-
-config_path=templates/camenduru
-function install_webui {
-    path=$config_path/webui.txt
-    if [[ -f $path ]]; then
-      mapfile -t lines < $path
-      if [[ ${#lines[@]} -ge 1 ]]; then
-        repo=$(echo "${lines[0]}" | cut -d "#" -f 1)
-        branch=$(echo "${lines[0]}" | grep -q "#" && echo "${lines[0]}" | cut -d "#" -f 2)
-        safe_git "$repo" "$BASEPATH" ${branch:+$branch}
-      else
-        echo "Error: wrong format for template config file: $path"
-      fi
-    else
-      safe_git https://github.com/AUTOMATIC1111/stable-diffusion-webui $BASEPATH
-    fi
-}
-#install_webui
-
-function install_embeddings {
-    path=$config_path/embeddings.txt
-    if [[ -f $path ]]; then
-      mapfile -t lines < $path
-      for line in "${lines[@]}"
-      do
-          repo=$(echo "${lines[0]}" | cut -d "#" -f 1)
-          branch=$(echo "${lines[0]}" | grep -q "#" && echo "${lines[0]}" | cut -d "#" -f 2)
-          safe_git "$repo" "$BASEPATH/embeddings" ${branch:+$branch}
-      done
-    fi
-}
-#install_embeddings
-
-function install_ESRGAN_models {
-    path=$config_path/ESRGAN_models.txt
-    if [[ -f $path ]]; then
-      mapfile -t lines < $path
-      for line in "${lines[@]}"
-      do
-          safe_fetch $line $BASEPATH/models/ESRGAN $(echo "$line" | awk -F/ '{print $NF}')
-      done
-    fi
-}
-
-function install_extra_scripts {
-    path=$config_path/extra_scripts.txt
-    if [[ -f $path ]]; then
-      mapfile -t lines < $path
-      for line in "${lines[@]}"
-      do
-          safe_fetch $line $BASEPATH/scripts $(echo "$line" | awk -F/ '{print $NF}')
-      done
-    fi
-}
-
-function install_extensions {
-    safe_git https://github.com/deforum-art/deforum-for-automatic1111-webui $BASEPATH/extensions/deforum-for-automatic1111-webui
-    path=$config_path/extensions.txt
-    if [[ -f $path ]]; then
-      mapfile -t lines < $path
-      for line in "${lines[@]}"
-      do
-          repo=$(echo "${lines[0]}" | cut -d "#" -f 1)
-          branch=$(echo "${lines[0]}" | grep -q "#" && echo "${lines[0]}" | cut -d "#" -f 2)
-          safe_git "$repo" "$BASEPATH/extensions" ${branch:+$branch}
-      done
-    fi
-}
-
-function install_cn_models {
-    safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11f1e_sd15_tile_fp16.safetensors
-    #safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11p_sd15_canny_fp16.safetensors
-    #safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11p_sd15_openpose_fp16.safetensors
-    #safe_fetch https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_seg_fp16.safetensors $BASEPATH/extensions/sd-webui-controlnet/models control_v11p_sd15_seg_fp16.safetensors
-}
-
-function install_checkpoints {
-    #Download Model & VAE
-    safe_fetch https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors $BASEPATH/models/Stable-diffusion Deliberate_v2.safetensors
-
-}
-
-function install_vae {
-    safe_fetch https://huggingface.co/Yukihime256/840000/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt $BASEPATH/models/VAE vae-ft-mse-840000-ema-pruned.ckpt
 }
 
 function prepare_pip_deps {
@@ -323,28 +235,20 @@ function prepare_fuse_dir {
     unionfs-fuse $BASEPATH/extensions/sd-webui-additional-networks/models/lora=RW:$BASEPATH/models/Lora=RW:/content/lora=RW /content/fused-lora
 }
 
-function prepare_clip {
-    mkdir -p $BASEPATH/models/CLIP
-    safe_fetch https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt $BASEPATH/models/CLIP ViT-L-14.pt
-}
-
 function install {
     #Prepare runtime
-    install_webui
-    install_embeddings
-    install_ESRGAN_models
-    install_extra_scripts
-    install_cn_models
-    install_extensions
-
-    install_from_template camenduru webui
-    install_from_template camenduru embeddings
-
+    component_types=( "webui" "extensions" "scripts" "embeddings" "ESRGAN_models" "checkpoints" "hypernetworks" "lora" "lycoris" "vae" "clip" "cn_models" )
+    for component_type in "${component_types[@]}"
+    do
+      template_path=$(pwd)/templates/camenduru
+      config_path=$template_path/$component_type.txt
+      echo $config_path
+      if [[ -f $config_path ]]; then
+        install_from_template $config_path
+      fi
+    done
 
     reset_repos $BASEPATH all
-
-    install_checkpoints
-    install_vae
 
     #Install Dependencies
     sed_for installation $BASEPATH
@@ -364,8 +268,6 @@ function run {
     #Prepare for running
     sed_for run $BASEPATH
 
-    prepare_clip
-
     cd $BASEPATH && python launch.py --listen --opt-sdp-attention --enable-insecure-extension-access --theme dark --gradio-queue --clip-models-path $BASEPATH/models/CLIP --multiple
 }
 
@@ -382,11 +284,11 @@ while getopts "f" opt; do
   esac
 done
 
-#Update packages
-#apt -y update -qq && apt -y install -qq unionfs-fuse libcairo2-dev pkg-config python3-dev
+Update packages
+apt -y update -qq && apt -y install -qq unionfs-fuse libcairo2-dev pkg-config python3-dev
 
-#if [ "$force" = true ] || [ ! -e "$BASEPATH/.install_status" ] || ! grep -qs "Installation Completed" "$BASEPATH/.install_status"; then
-#    install
-#fi
+if [ "$force" = true ] || [ ! -e "$BASEPATH/.install_status" ] || ! grep -qs "Installation Completed" "$BASEPATH/.install_status"; then
+    install
+fi
 
-#run
+run
