cfa0ea084f10daa081acb1dba7a2c17ca2a3bd9f
larui762 <46641973+larui762@users.noreply.github.com>
2023-08-13 19:49:59 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -221,6 +221,7 @@ function install_from_template {
 function prepare_pip_deps {
     pip install -q torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U
     pip install -q xformers==0.0.18 triton==2.0.0 -U
+    pip install torchmetrics==0.11.4
 }
 
 function prepare_fuse_dir {
